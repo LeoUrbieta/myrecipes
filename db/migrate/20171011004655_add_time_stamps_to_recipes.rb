129348904e5ec9ac60fbf31fd8333b942d5e67c2
Leo Moreno <achicopalado@gmail.com>
2017-10-12 00:38:47 +0000

    add chef and recipe model validations with tests

---
+++ db/migrate/20171011004655_add_time_stamps_to_recipes.rb
@@ -0,0 +1,6 @@
+class AddTimeStampsToRecipes < ActiveRecord::Migration[5.1]
+  def change
+    add_column :recipes, :created_at, :datetime
+    add_column :recipes, :updated_at, :datetime
+  end
+end
