69b4c17a5ec567551e98dded547b4143d2899c26
Leo Moreno <achicopalado@gmail.com>
2017-10-20 00:23:42 +0000

    Completed homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'pages/home', to: 'pages#home'
   
   get '/recipes', to: 'recipes#index'
-  
+  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
