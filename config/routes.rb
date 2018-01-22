d0384ffcb8e854aab05a9bc19e54f905b0fa5be3
Leo Moreno <achicopalado@gmail.com>
2018-01-22 22:23:01 +0000

    Make comments real-time

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   
   resources :ingredients, excpet: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ActionCable.server => '/cable'
+  
 end
